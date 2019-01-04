edfba9687d3e53a22baada2ffaea7338d86d5191
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2019-01-04 15:05:51 +0200

    domf: add first boot script into aos-servicemanager
    First boot script used to enable disk quotas
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- recipes-domu/domu-image-fusion/files/meta-xt-prod-extra/recipes-aos/aos-servicemanager/files/first_boot.sh
+++ recipes-domu/domu-image-fusion/files/meta-xt-prod-extra/recipes-aos/aos-servicemanager/files/first_boot.sh
@@ -8,11 +8,14 @@ echo "Enable disk quotas"
 
 quotacheck -avum && quotaon -avu
 
+<<<<<<< HEAD
 # Update certificates
 
 echo "Update certificates"
 
 update-ca-certificates
 
+=======
+>>>>>>> 124a3d3... domf: add first boot script into aos-servicemanager
 # Disable first boot service
 systemctl disable first_boot.service
