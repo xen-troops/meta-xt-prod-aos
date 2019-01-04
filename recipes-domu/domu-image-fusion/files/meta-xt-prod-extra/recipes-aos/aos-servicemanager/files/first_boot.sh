650ea24a0edcf5df2aa31f58838b1fbb47212919
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2019-01-04 15:44:36 +0200

    domf: add update ca certificates into first boot script
    It is required to use aos/rootCA.crt
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- recipes-domu/domu-image-fusion/files/meta-xt-prod-extra/recipes-aos/aos-servicemanager/files/first_boot.sh
+++ recipes-domu/domu-image-fusion/files/meta-xt-prod-extra/recipes-aos/aos-servicemanager/files/first_boot.sh
@@ -8,5 +8,11 @@ echo "Enable disk quotas"
 
 quotacheck -avum && quotaon -avu
 
+# Update certificates
+
+echo "Update certificates"
+
+update-ca-certificates
+
 # Disable first boot service
 systemctl disable first_boot.service
