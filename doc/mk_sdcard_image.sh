d30c1e4cf3dbc5939f4dde1578fc62c4b408c348
Ruslan Shymkevych <ruslan_shymkevych@epam.com>
2019-02-15 11:21:37 +0200

    mk_sdcard_image: Clean up not needed code
    Remove not used parameters of functions.
    
    Remove not needed global variables DOM*_PRESENT.
    This variable is set only if parameters of partition are defined.
    So if DOM*_START is set, this mean that partition is present.
    
    Remove creation of not needed loop devices. Now we create one loop
    device for image output, and one for android's disk.
    For now android's loop device is created/deleted two times: for
    partitioning, and for writing images. To fix this we need to
    significantly rework script, so effort is to big for small reward.
    
    Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
    Reviewed-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- doc/mk_sdcard_image.sh
+++ doc/mk_sdcard_image.sh
@@ -221,14 +221,6 @@ mkfs_domf()
 	mkfs_one $1 $DOMF_PARTITION $DOMF_LABEL
 }
 
-mkfs_domf()
-{
-	local img_output_file=$1
-	local loop_dev=$2
-
-	mkfs_one $img_output_file $loop_dev 3 domf
-}
-
 mkfs_doma()
 {
 	# Below we use 4 as number of partition inside android's partition.
@@ -239,14 +231,6 @@ mkfs_doma()
 mkfs_domu()
 {
 	mkfs_one $1 $DOMU_PARTITION $DOMU_LABEL
-}
-
-mkfs_domu()
-{
-	local img_output_file=$1
-	local loop_dev=$2
-
-	mkfs_one $img_output_file $loop_dev 3 domu
 }
 
 mkfs_image()
@@ -389,22 +373,20 @@ unpack_domf()
 {
 	local db_base_folder=$1
 	local loop_dev=$2
-	local img_output_file=$3
 
 	print_step  "Unpacking DomF"
 
-	unpack_dom_from_tar $db_base_folder $loop_dev $img_output_file 3 domu
+	unpack_dom_from_tar $db_base_folder $loop_dev 3 domu
 }
 
 unpack_domu()
 {
 	local db_base_folder=$1
 	local loop_dev=$2
-	local img_output_file=$3
 
 	print_step  "Unpacking DomU"
 
-	unpack_dom_from_tar $db_base_folder $loop_dev $img_output_file 3 domu
+	unpack_dom_from_tar $db_base_folder $loop_dev 3 domu
 }
 
 unpack_doma()
@@ -515,16 +497,6 @@ unpack_domain()
 		domu)
 			mkfs_domu $img_output_file
 			unpack_domu $db_base_folder $img_output_file
-		;;
-		domf)
-			loop_dev=`sudo losetup --find --partscan --show $img_output_file`
-			mkfs_domf $img_output_file $loop_dev
-			unpack_domf $db_base_folder $loop_dev $img_output_file
-		;;
-		domu)
-			loop_dev=`sudo losetup --find --partscan --show $img_output_file`
-			mkfs_domu $img_output_file $loop_dev
-			unpack_domu $db_base_folder $loop_dev $img_output_file
 		;;
 		doma)
 			local loop_dev_a=`sudo losetup --find --partscan --show ${img_output_file}p$DOMA_PARTITION`
