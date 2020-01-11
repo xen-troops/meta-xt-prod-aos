xen-troops/meta-xt-prod-aos

commit 788283d027a3cac0cc8807f72671c004033b3815

dom0: provisioning: Create model_name.txt
For provisioning purposes we need to have /etc/aos/model_name.txt
file with some info that allows to distinct 'device family'.
This path uses XT_CANONICAL_MACHINE_NAME as model name.
See meta-xt-images/machine/meta-xt-images-rcar-gen3/conf/machine/*
for possible values of machine(model) name.

Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

recipes-dom0/dom0-image-thin-initramfs/files/meta-xt-prod-extra/recipes-extended/provisioning/provisioning.bb
@@ -20,4 +20,7 @@ do_install() {
     install -d ${D}${base_prefix}${XT_DIR_ABS_ROOTFS_SCRIPTS}
     install -m 0755 ${S}/aos-provisioning.step1.sh ${D}${base_prefix}${XT_DIR_ABS_ROOTFS_SCRIPTS}
     install -m 0755 ${S}/aos-provisioning.step2.sh ${D}${base_prefix}${XT_DIR_ABS_ROOTFS_SCRIPTS}
+
+    install -d ${sysconfdir}/aos
+    echo ${XT_CANONICAL_MACHINE_NAME} > ${sysconfdir}/aos/model_name.txt
 }

commit 1e6f8ca2e9dad497cea86a0b0d88d7bee3187062

dom0: provisioning: Fix path to model_name.txt
Fixes: 788283d ("dom0: provisioning: Create model_name.txt")

Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>

recipes-dom0/dom0-image-thin-initramfs/files/meta-xt-prod-extra/recipes-extended/provisioning/provisioning.bb
@@ -21,6 +21,6 @@ do_install() {
     install -m 0755 ${S}/aos-provisioning.step1.sh ${D}${base_prefix}${XT_DIR_ABS_ROOTFS_SCRIPTS}
     install -m 0755 ${S}/aos-provisioning.step2.sh ${D}${base_prefix}${XT_DIR_ABS_ROOTFS_SCRIPTS}
 
-    install -d ${sysconfdir}/aos
-    echo ${XT_CANONICAL_MACHINE_NAME} > ${sysconfdir}/aos/model_name.txt
+    install -d ${D}${sysconfdir}/aos
+    echo ${XT_CANONICAL_MACHINE_NAME} > ${D}${sysconfdir}/aos/model_name.txt
 }